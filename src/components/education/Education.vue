58b273302429611997da4bc5126aae9e8f4cdf6d
lavank179 <chlavankumar179@gmail.com>
2021-06-06 12:05:38 +0530

    Education completed

--- src/components/education/Education.vue
+++ src/components/education/Education.vue
@@ -4,18 +4,21 @@
       <v-col lg="1" md="1" sm="1" cols="1"></v-col>
       <v-col lg="10" md="10" sm="10" cols="10">
         <div id="content">
-          <v-row v-for="(val, key) in edu" :key="key" id="rows">
-            <v-col lg="10" md="10" sm="10" cols="12" align="left">
-              <p>{{ key }}, {{ val.city }}</p>
-              <p>{{ val.Degree }}</p>
-              <br />
-              <p>{{ val.Department }}</p>
-              <p>Secured : {{ val.Marks }}</p>
-            </v-col>
-            <v-col lg="2" md="2" sm="2" cols="12" align="left">
-              <h4>{{ val.Duration }}</h4>
-            </v-col>
-          </v-row>
+          <div v-for="(val, key) in edu" :key="key" id="rows">
+            <v-row>
+              <v-col lg="10" md="10" sm="10" cols="12" align="left">
+                <p>{{ key }}, {{ val.city }}</p>
+                <p>{{ val.Degree }}</p>
+                <br />
+                <p>{{ val.Department }}</p>
+                <p>Secured : {{ val.Marks }}</p>
+              </v-col>
+              <v-col lg="2" md="2" sm="2" cols="12" align="left">
+                <h4>{{ val.Duration }}</h4>
+              </v-col>
+            </v-row>
+            <hr>
+          </div>
         </div>
       </v-col>
       <v-col lg="1" md="1" sm="1" cols="1"></v-col>
