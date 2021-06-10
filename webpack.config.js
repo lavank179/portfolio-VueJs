d2d8ff17f9ffe8172865ca038d7ea930f1edd2f2
lavank179 <chlavankumar179@gmail.com>
2021-06-10 23:42:54 +0530

    All pages - 1

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['babel-preset-env']
         },
         exclude: /node_modules/
       },
